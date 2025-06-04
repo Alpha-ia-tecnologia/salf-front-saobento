f2df23c558adbca0b8873e24bc3b14d3178ff795
Alpha-ia-tecnologia <alpha.ia.tecnologia@gmail.com>
2025-06-04 07:37:29 -0300

    Update modalResult.js

--- pages/avaliacao/modalResult.js
+++ pages/avaliacao/modalResult.js
@@ -9,7 +9,7 @@ novoAvaliacao.addEventListener("click",() => {
     location.reload()
 })
 voltar.addEventListener('click',() => {
-    window.location.href = location.origin + '/pages/dashboard/index.html'
+    window.location.href = location.origin + 'pages/dashboard/dashboard.html'
 })
 
 modalResult.addEventListener('click',async () => {
