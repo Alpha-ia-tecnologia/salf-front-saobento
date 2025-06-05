6eaf0e3ceabba07024e110f1bca64048ebc26f00
veetorio <ettorevitorio98@gmail.com>
2025-06-05 12:47:12 -0300

    fix dash

--- js/dashboard.js
+++ js/dashboard.js
@@ -237,8 +237,8 @@ function PopularGraphSeries(obj) {
 }
 let year = null;
 function PopularGraphYear({ previousYear, currentYear }) {
-    const labels = [ currentYear.year , previousYear.year];
-    const data = [currentYear.total,previousYear.total];
+    const labels = [previousYear.year ,currentYear.year ];
+    const data = [ previousYear.total , currentYear.total];
     if (data.length === 0) {
         return;
     }
@@ -247,7 +247,7 @@ function PopularGraphYear({ previousYear, currentYear }) {
     }
     year = new Chart(canvaGraphBar, {
         type: 'bar',
-        data: { labels, datasets: [{ label: 'Total', data : [10] }] },
+        data: { labels, datasets: [{ label: 'Total', data : data }] },
         options: {
             responsive: true,
             maintainAspectRatio: false,
