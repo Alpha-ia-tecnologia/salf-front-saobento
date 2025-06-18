c9f1c6b6fc3c1bf04e099d2d1fed77c05491a32e
veetorio <ettorevitorio98@gmail.com>
2025-06-18 11:44:07 -0300

    ranking regioes

--- pages/rankings-e/rankings.js
+++ pages/rankings-e/rankings.js
@@ -1,15 +1,24 @@
-const chartSeries = document.getElementById('chart-series');
+const chartProgressao = document.getElementById('chart-progressao');
 const btnFiltrar = document.getElementById('btn-filtrar');
 let filters = false
 document.addEventListener('DOMContentLoaded', async () => {
     const response = await fetch(`https://salf-salf-api2.gkgtsp.easypanel.host/api/dashboard/school-ranking`, {
         method: 'GET',
         headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
-    }
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+        }
+    });
+    const response2 = await fetch(`https://salf-salf-api2.gkgtsp.easypanel.host/api/dashboard/ranking-by-region`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+        }
     });
     const data = await response.json();
+    const data2 = await response2.json();
+    randerChartProgressao(data2);
     randerRankingsEscola(data.data);
 })
 // btnFiltrar.addEventListener('click', async (e) => {
@@ -20,6 +29,58 @@ document.addEventListener('DOMContentLoaded', async () => {
 //     const evento = document.getElementById('evento').value;
 
 // });
+
+
+
+const randerChartProgressao = (object) => {
+    const labelsExternos = object.map(item => item.region);
+    const datasetsInterno = object.map(item => {
+        return {
+            label: item.region,
+            data: item.percentage,
+        }
+    });
+    const ctx = chartProgressao.getContext('2d');
+    new Chart(ctx, {
+        type: 'bar',
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    suggestedMax: 100,
+                    ticks: {
+                        stepSize: 20,
+                        callback: function (value, index, values) {
+                            return value + '%';
+                        }
+                    },
+                }
+            }
+        },
+
+        plugins: {
+            legend: { display: false },
+            tooltip: {
+                callbacks: {
+                    label: ctx => `Total: ${ctx.raw}`
+                }
+            }
+        },
+        data: {
+            labels: labelsExternos,
+            datasets: [{
+                label: 'Regiões com escolas com maior numêro de alunos com fluência',
+                data: datasetsInterno.map(item => item.data),
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+        }
+    });
+}
+
 
 const randerRankingsEscola = (data) => {
     const body = document.getElementById('tbody');
