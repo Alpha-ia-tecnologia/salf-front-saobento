b7150a49ab823b8f6d43ee98ec07368527067c8e
veetorio <ettorevitorio98@gmail.com>
2025-06-23 18:21:56 -0300

    pom.xml

--- pages/rankings-e/rankings.js
+++ pages/rankings-e/rankings.js
@@ -21,14 +21,22 @@ document.addEventListener('DOMContentLoaded', async () => {
     randerChartProgressao(data2);
     randerRankingsEscola(data.data);
 })
-// btnFiltrar.addEventListener('click', async (e) => {
-//     e.preventDefault();
-//     const regiao = document.getElementById('filtro-regiao').value;
-//     const grupo = document.getElementById('filtro-grupo').value;
-//     const escola = document.getElementById('filtro-escola').value;
-//     const evento = document.getElementById('evento').value;
-
-// });
+btnFiltrar.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const regiao = document.getElementById('filtro-regiao').value;
+    const grupo = document.getElementById('filtro-grupo').value;
+    const escola = document.getElementById('filtro-escola').value;
+    const evento = document.getElementById('evento').value;
+    const response = await fetch(`https://salf-salf-api2.gkgtsp.easypanel.host/api/dashboard/school-ranking?region=${regiao}&group=${grupo}&school=${escola}&event=${evento}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+        }
+    });
+    const data = await response.json(); 
+    randerRankingsEscola(data.data);
+});
 
 
 
